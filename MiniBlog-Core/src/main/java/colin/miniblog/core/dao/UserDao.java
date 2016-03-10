4e28423c845ddee8ba8484d74f487ee0beb0fb42
linqiang <liqniang@raycloud.com>
2016-03-10 18:42:47 +0800

    初始化工程结构

---
+++ MiniBlog-Core/src/main/java/colin/miniblog/core/dao/UserDao.java
@@ -0,0 +1,14 @@
+package colin.miniblog.core.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * 创建人 LinQiang
+ * 创建日期 2016/3/10
+ * 项目名称 MiniBlog
+ * 当前包名 colin.miniblog.core.dao
+ */
+@Repository
+public class UserDao {
+
+}
---
+++ MiniBlog-Core/src/main/java/colin/miniblog/core/dao/common/CommonDao.java
@@ -0,0 +1,17 @@
+package colin.miniblog.core.dao.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 创建人 LinQiang
+ * 创建日期 2016/3/10
+ * 项目名称 MiniBlog
+ * 当前包名 colin.miniblog.core.dao
+ */
+@Repository
+public class CommonDao {
+
+  @Autowired
+  private
+}
